1121aae0840344014967684aabe5582504782e76
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2023-11-18 13:29:49 +0000

    JustAddedDifferentMovement

---
+++ Assets/Scripts_Test/PlayerMovemenet_Free_TEST.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class PlayerMovmenetFreeTest : MonoBehaviour
+{
+    public CharacterController characterController;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+    public bool isMoving;
+    void Start()
+    {
+        isMoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        isMoving = true;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            characterController.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        else
+        {
+            isMoving = false;
+        }
+    }
+}
+
