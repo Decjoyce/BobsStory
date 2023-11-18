2e770c7b5b92b99a121a6e81aed660b3f53e776e
AAROnmn <aaronmccabe212@gmail.com>
2023-11-18 12:29:20 +0000

    AudioTest

---
+++ Assets/Scripts_Test/PlayerMovement_Grid_Test.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_Grid_Test : MonoBehaviour
+{
+    [SerializeField] int speed;
+    [SerializeField] GameObject graphics;
+    
+    [Header("SoundEffects")]
+    [SerializeField] GameObject footsteps;
+
+    private void Start()
+    {
+        //Audio
+        footsteps.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //Audio
+            footstep();
+            
+            //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);
+            transform.Translate(Vector3.forward * speed);
+            graphics.transform.rotation = Quaternion.Euler(0, 0, 0);
+            
+            //AudioStop
+            Invoke(nameof(StopFootSteps), 1);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //Audio
+            footstep();
+            
+            //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - speed);
+            transform.Translate(-Vector3.forward * speed);
+            graphics.transform.rotation = Quaternion.Euler(0, 180, 0);
+            
+            //AudioStop
+            Invoke(nameof(StopFootSteps), 1);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //Audio
+            footstep();
+            
+            graphics.transform.rotation = Quaternion.Euler(0, 90, 0);
+            
+            //AudioStop
+            Invoke(nameof(StopFootSteps), 1);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //Audio
+            footstep();
+            
+            graphics.transform.rotation = Quaternion.Euler(0, -90, 0);
+            
+            //AudioStop
+            Invoke(nameof(StopFootSteps),1);
+        }
+        
+    }
+
+    void footstep()
+    {
+        //Play Audio
+        footsteps.SetActive(true);
+    }
+
+    void StopFootSteps()
+    {
+        //Stop Audio
+        footsteps.SetActive(false);
+    }
+
+    
+}
