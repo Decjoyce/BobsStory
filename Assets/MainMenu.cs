c69344eca0248869774c91eb78a0eb0dd8513cfd
LilGenius03 <finnarcher03@gmail.com>
2023-12-10 18:05:29 +0000

    Main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,15 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadLevel()
+    {
+        int buildIndex = 1;
+
+        SceneManager.LoadScene(buildIndex);
+    }
+}
